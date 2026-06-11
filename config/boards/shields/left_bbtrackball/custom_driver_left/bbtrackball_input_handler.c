649eab846ca837df9fb82aa8b779a87d9d6c2a2a
siegfriedwhisper <46621235+siegfriedwhisper@users.noreply.github.com>
2026-06-12 01:06:45 +0800

    feat: swap trackball modes - default=scroll, space/capslock=cursor

--- config/boards/shields/left_bbtrackball/custom_driver_left/bbtrackball_input_handler.c
+++ config/boards/shields/left_bbtrackball/custom_driver_left/bbtrackball_input_handler.c
@@ -267,13 +267,13 @@ static void bbtrackball_work_handler(struct k_work *work) {
     }
 
     if (space_pressed || capslock) {
-        input_report_rel(dev, INPUT_REL_HWHEEL, -dx, false, K_NO_WAIT);
-        input_report_rel(dev, INPUT_REL_WHEEL, dy, true, K_NO_WAIT);
+        input_report_rel(dev, INPUT_REL_X, -dx, false, K_NO_WAIT);
+        input_report_rel(dev, INPUT_REL_Y, -dy, true, K_NO_WAIT);
         return;
     }
 
-    input_report_rel(dev, INPUT_REL_X, -dx, false, K_NO_WAIT);
-    input_report_rel(dev, INPUT_REL_Y, -dy, true, K_NO_WAIT);
+    input_report_rel(dev, INPUT_REL_HWHEEL, -dx, false, K_NO_WAIT);
+    input_report_rel(dev, INPUT_REL_WHEEL, dy, true, K_NO_WAIT);
 }
 
 /* =========================================================
